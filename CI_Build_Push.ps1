8adee2749b4c7d543dc02d054439f0fedcf4b6dc
BlaiseD <btaylor@bPolicySolutions.com>
2020-07-19 10:45:25 -0400

    Should be ready

--- CI_Build_Push.ps1
+++ CI_Build_Push.ps1
@@ -4,10 +4,7 @@ echo $Env:REPO_OWNER
 echo $Env:REPO
 echo $Env:PROJECT_PATH
 echo $Env:NUGET_PACKAGE_PATH
-if ($Env:REPO_OWNER -ne "BlaiseD") {
-    Write-Host "${scriptName}: Runs on BlaiseD repositories."
-} else {
-    dotnet build $Env:PROJECT_PATH --configuration Release
-    dotnet pack $Env:PROJECT_PATH -c Release -o .\artifacts --no-build
-    dotnet nuget push $Env:NUGET_PACKAGE_PATH --skip-duplicate
-}
+
+dotnet build $Env:PROJECT_PATH --configuration Release
+dotnet pack $Env:PROJECT_PATH -c Release -o .\artifacts --no-build
+dotnet nuget push $Env:NUGET_PACKAGE_PATH --skip-duplicate
--- CI_Pack_Push.ps1
+++ CI_Pack_Push.ps1
@@ -4,9 +4,6 @@ echo $Env:REPO_OWNER
 echo $Env:REPO
 echo $Env:PROJECT_PATH
 echo $Env:NUGET_PACKAGE_PATH
-if ($Env:REPO_OWNER -ne "BlaiseD") {
-    Write-Host "${scriptName}: Runs on BlaiseD repositories."
-} else {
-    dotnet pack $Env:PROJECT_PATH -c Release -o .\artifacts --no-build
-    dotnet nuget push $Env:NUGET_PACKAGE_PATH --skip-duplicate
-}
+
+dotnet pack $Env:PROJECT_PATH -c Release -o .\artifacts --no-build
+dotnet nuget push $Env:NUGET_PACKAGE_PATH --skip-duplicate
