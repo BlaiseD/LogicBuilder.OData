5af42087e517fddd747fa85e6f2042460bf8e9fd
BlaiseD <btaylor@bPolicySolutions.com>
2020-07-19 10:17:49 -0400

    Set all variables in .yml

--- CI_Build_Push.ps1
+++ CI_Build_Push.ps1
@@ -1,14 +1,12 @@
 $scriptName = $MyInvocation.MyCommand.Name
 
-echo 'Owner $($Env:REPO_OWNER)' 
-echo 'Repo $($Env:REPO)'
+echo $Env:REPO_OWNER
+echo $Env:REPO
+echo $Env:PROJECT_PATH
+echo $Env:NUGET_PACKAGE_PATH
 if ($Env:REPO_OWNER -ne "BlaiseD") {
     Write-Host "${scriptName}: Runs on BlaiseD repositories."
 } else {
-    echo '::set-env name=PROJECT_PATH::.\${{ env.PROJECT_NAME }}\${{ env.PROJECT_NAME }}.csproj'
-    echo '::set-env name=NUGET_PACKAGE_PATH::.\artifacts\${{ env.PROJECT_NAME }}.${{ env.VERSION_NUMBER }}.nupkg'
-    echo $Env:PROJECT_PATH
-    echo $Env:NUGET_PACKAGE_PATH
     dotnet build $Env:PROJECT_PATH --configuration Release
     dotnet pack $Env:PROJECT_PATH -c Release -o .\artifacts --no-build
     dotnet nuget push $Env:NUGET_PACKAGE_PATH --skip-duplicate
