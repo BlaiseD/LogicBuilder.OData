e5b6f0bdf01b74cce9f88b0d30b7bf7379fd4687
BlaiseD <btaylor@bPolicySolutions.com>
2020-07-19 09:13:50 -0400

    pwsh

---
+++ CI_Build_Push.ps1
@@ -0,0 +1,15 @@
+$scriptName = $MyInvocation.MyCommand.Name
+
+echo github.repository_owner
+echo github.repository
+if (github.repository_owner -ne "BlaiseD") {
+    Write-Host "${scriptName}: Runs on BlaiseD repositories."
+} else {
+    echo '::set-env name=PROJECT_PATH::.\${{ env.PROJECT_NAME }}\${{ env.PROJECT_NAME }}.csproj'
+    echo '::set-env name=NUGET_PACKAGE_PATH::.\artifacts\${{ env.PROJECT_NAME }}.${{ env.VERSION_NUMBER }}.nupkg'
+    echo $Env.PROJECT_PATH
+    echo $Env.NUGET_PACKAGE_PATH
+    dotnet build $Env:PROJECT_PATH --configuration Release
+    dotnet pack $Env:PROJECT_PATH -c Release -o .\artifacts --no-build
+    dotnet nuget push $Env:NUGET_PACKAGE_PATH --skip-duplicate
+}
--- Release_Pack_Push.ps1
+++ Release_Pack_Push.ps1
@@ -9,4 +9,6 @@ if (github.repository_owner -ne "BlaiseD") {
     echo '::set-env name=NUGET_PACKAGE_PATH::.\artifacts\${{ env.PROJECT_NAME }}.${{ env.VERSION_NUMBER }}.nupkg'
     echo $Env.PROJECT_PATH
     echo $Env.NUGET_PACKAGE_PATH
+    dotnet pack $Env:PROJECT_PATH -c Release -o .\artifacts --no-build
+    dotnet nuget push $Env:NUGET_PACKAGE_PATH --skip-duplicate --source $Env:NUGET_URL --api-key $Env:NUGET_API_KEY
 }
