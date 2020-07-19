c9d7d68a223d91438960e45bdd1f1c3c4759afe3
BlaiseD <btaylor@bPolicySolutions.com>
2020-07-19 10:06:45 -0400

    Sysntax is important

--- CI_Pack_Push.ps1
+++ CI_Pack_Push.ps1
@@ -1,14 +1,14 @@
 $scriptName = $MyInvocation.MyCommand.Name
 
-echo github.repository_owner
-echo github.repository
-if (github.repository_owner -ne "BlaiseD") {
+echo 'Owner $($Env:REPO_OWNER)' 
+echo 'Repo $($Env:REPO)'
+if ($Env:REPO_OWNER -ne "BlaiseD") {
     Write-Host "${scriptName}: Runs on BlaiseD repositories."
 } else {
     echo '::set-env name=PROJECT_PATH::.\${{ env.PROJECT_NAME }}\${{ env.PROJECT_NAME }}.csproj'
     echo '::set-env name=NUGET_PACKAGE_PATH::.\artifacts\${{ env.PROJECT_NAME }}.${{ env.VERSION_NUMBER }}.nupkg'
-    echo $Env.PROJECT_PATH
-    echo $Env.NUGET_PACKAGE_PATH
+    echo $Env:PROJECT_PATH
+    echo $Env:NUGET_PACKAGE_PATH
     dotnet pack $Env:PROJECT_PATH -c Release -o .\artifacts --no-build
     dotnet nuget push $Env:NUGET_PACKAGE_PATH --skip-duplicate
 }
--- Release_Pack_Push.ps1
+++ Release_Pack_Push.ps1
@@ -1,14 +1,14 @@
 $scriptName = $MyInvocation.MyCommand.Name
 
-echo 'Owner ${{Env.REPO_OWNER}}'
-echo 'Repo ${{Env.REPO}}'
+echo 'Owner $($Env:REPO_OWNER)' 
+echo 'Repo $($Env:REPO)'
 if ($Env.REPO_OWNER -ne "BlaiseD") {
     Write-Host "${scriptName}: Runs on BlaiseD repositories."
 } else {
     echo '::set-env name=PROJECT_PATH::.\${{ env.PROJECT_NAME }}\${{ env.PROJECT_NAME }}.csproj'
     echo '::set-env name=NUGET_PACKAGE_PATH::.\artifacts\${{ env.PROJECT_NAME }}.${{ env.VERSION_NUMBER }}.nupkg'
-    echo $Env.PROJECT_PATH
-    echo $Env.NUGET_PACKAGE_PATH
+    echo $Env:PROJECT_PATH
+    echo $Env:NUGET_PACKAGE_PATH
     dotnet pack $Env:PROJECT_PATH -c Release -o .\artifacts --no-build
     dotnet nuget push $Env:NUGET_PACKAGE_PATH --skip-duplicate --source $Env:NUGET_URL --api-key $Env:NUGET_API_KEY
 }
