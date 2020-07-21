96f480e185870180992919b7006a6eeeecd2f864
BlaiseD <btaylor@bPolicySolutions.com>
2020-07-21 08:23:35 -0400

    Testing minver

--- CI_Build_Push.ps1
+++ CI_Build_Push.ps1
@@ -1,12 +1,38 @@
-Write-Host $Env:REPO_OWNER
-Write-Host $Env:REPO
+Write-Host "Owner ${$Env:REPO_OWNER}"
+Write-Host "Repository ${$Env:REPO}"
+
+
 
 $PROJECT_PATH = ".\$($Env:PROJECT_NAME)\$($Env:PROJECT_NAME).csproj"
-$NUGET_PACKAGE_PATH = ".\artifacts\$($Env:PROJECT_NAME).$($Env:VERSION_NUMBER).nupkg"
 
-Write-Host $PROJECT_PATH 
-Write-Host $NUGET_PACKAGE_PATH
+
+
+
+#ProductMajorPart   : 6
+#ProductMinorPart   : 1
+#ProductBuildPart   : 7601
+#ProductPrivatePart : 17767
+
+Write-Host "Project Path ${$PROJECT_PATH}"
+Write-Host "Package Path ${$NUGET_PACKAGE_PATH}"
 
 dotnet build $PROJECT_PATH --configuration Release
-dotnet pack $PROJECT_PATH -c Release -o .\artifacts --no-build
-dotnet nuget push $NUGET_PACKAGE_PATH --skip-duplicate
+
+$DLL_PATH = ".\$($Env:PROJECT_NAME)\bin\Debug\netstandard2.0\$($Env:PROJECT_NAME).csproj"
+
+$item = (get-item $DLL_PATH)
+Write-Host (get-item $DLL_PATH).VersionInfo.ProductMajorPart
+Write-Host (get-item $DLL_PATH).VersionInfo.ProductMinorPart
+Write-Host (get-item $DLL_PATH).VersionInfo.ProductBuildPart
+Write-Host (get-item $DLL_PATH).VersionInfo.ProductPrivatePart
+
+$version = "$($item.VersionInfo.ProductMajorPart).$($item.VersionInfo.ProductMinorPart).$($item.VersionInfo.ProductBuildPart)" 
+
+$NUGET_PACKAGE_PATH = ".\artifacts\$($Env:PROJECT_NAME).$($item).nupkg"
+
+if ($Env:REPO_OWNER -ne "BlaiseD") {
+    Write-Host "${scriptName}: Runs on BlaiseD repositories."
+} else {
+    dotnet pack $PROJECT_PATH -c Release -o .\artifacts --no-build
+    dotnet nuget push $NUGET_PACKAGE_PATH --skip-duplicate
+}
--- CI_Pack_Push.ps1
+++ CI_Pack_Push.ps1
@@ -7,5 +7,9 @@ $NUGET_PACKAGE_PATH = ".\artifacts\$($Env:PROJECT_NAME).$($Env:VERSION_NUMBER).n
 Write-Host $PROJECT_PATH 
 Write-Host $NUGET_PACKAGE_PATH
 
-dotnet pack $PROJECT_PATH -c Release -o .\artifacts --no-build
-dotnet nuget push $NUGET_PACKAGE_PATH --skip-duplicate
+if ($Env:REPO_OWNER -ne "BlaiseD") {
+    Write-Host "${scriptName}: Only create packages on BlaiseD repositories."
+} else {
+    dotnet pack $PROJECT_PATH -c Release -o .\artifacts --no-build
+    dotnet nuget push $NUGET_PACKAGE_PATH --skip-duplicate
+}
